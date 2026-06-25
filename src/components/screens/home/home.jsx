641d4ff9e1d0dd705dd7829e237633401516be7b
Raja <raja.rathinam@pcisage.com>
2026-06-25 16:12:56 +0530

    update

--- src/components/screens/home/home.jsx
+++ src/components/screens/home/home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import HeroSection from "./sections/hero/Hero";
-// import ImpactSection from "./sections/impact/Impact";
-import AboutSection from "./sections/about/About";
+import ImpactSectionx from "./sections/impact/impact";
+import AboutSection from "./sections/about/about";
 import WhatWeDoSection from "./sections/what_we_do/WhatWeDo";
 import FounderSpotlightSection from "./sections/founder_spotlight/FounderSpotlight";
 import WenbaSection from "./sections/wenba_teaser/Wenba";
-import MembershipSection from "./sections/membership/Membership";
-
-import CtaBannerSection from "./sections/cta_banner/CtaBanner";
-import ImpactSectionx from "./sections/impact/impact";
+import MembershipSection from "./sections/membership/membership";
 import TestimonialsSection from "./sections/testimonials/testimonials";
+import CtaBannerSection from "./sections/cta_banner/CtaBanner";
+// import ImpactSection from "./sections/impact/Impact";
+
 
 const HomeScreen = () => {
   return (
